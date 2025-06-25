0e9c2a1f343803a963edf40823a6d8627101eb75
abhii-raj <abhiiraj021@gmail.com>
2025-06-26 02:01:53 +0530

    All crud operation

---
+++ models/campground.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const campgroundSchema = new mongoose.Schema(
+    {
+        title: String,
+        price: String,
+        description: String,
+        location: String
+
+    }
+);
+
+module.exports = mongoose.model('Campground' , campgroundSchema);
