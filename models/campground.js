f78da27a0776764e135e46d2e756b3cd917b7acc
abhii-raj <abhiiraj021@gmail.com>
2025-06-26 23:20:33 +0530

    adding css

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const campgroundSchema = new mongoose.Schema(
     {
         title: String,
+        image: String,
         price: String,
         description: String,
         location: String
